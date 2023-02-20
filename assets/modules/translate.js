cf46ac6dc4cef0a8b0916fffbed344f9234659ba
Pavel <you@example.com>
2023-02-20 19:39:59 +0300

    feat: make translation mode (20.02.23 19:39)

---
+++ assets/modules/translate.js
@@ -0,0 +1,48 @@
+const i18Obj = {
+    'en': {
+        'home-text': 'home',
+        'work-text': 'work',
+        'menu-about-text': 'about',
+        'menu-contact-text': 'contact',
+        'name-text': 'Pavel Terno',
+        'subname-text': 'Junior Frontend developer',
+
+        'proj1-text': 'Shrek Game Puzzle',
+        'proj2-text': 'Shelter Site',
+        'proj3-text': 'Audio Player',
+        'proj4-text': 'Virtual Keyboard',
+        'proj5-text': 'Online Zoo Site',
+        'proj6-text': 'Meme Slider',
+        'proj7-text': '<span class="YBspan">YouTube</span> Channel',
+
+        'about-text': 'About',
+        'preview-text': 'Abakkus is a collection of projects featuring work specializing in UI/UX/Graphic Design, Illustration & Motion Graphics. I am passionate about the concept and creation of interactive experiences, and I am convinced that clear usability in combination with stimulating graphics can deliver a compelling message. Thus far, I feel fortunate to have had the opportunity to have worked with amazingly talented people on truely inspiring projects. Through these collaborations I have been able to gain a breadth of insights and knowledge that I would like to share with you here and on your next project.',
+        'contact-text': 'Contact',
+
+        'designed-text': 'Designed by Pavel Terno'
+    },
+    'ru': {
+        'home-text': 'домой',
+        'work-text': 'работы',
+        'menu-about-text': 'обо мне',
+        'menu-contact-text': 'контакты',
+        'name-text': 'Павел Терно',
+        'subname-text': 'Junior Frontend разработчик',
+
+        'proj1-text': 'Игра "Шрек-пятнашки"',
+        'proj2-text': 'Сайт "Приют"',
+        'proj3-text': 'Аудиоплеер',
+        'proj4-text': 'Виртуальная клавиатура',
+        'proj5-text': 'Сайт "Онлайн зоопарк"',
+        'proj6-text': 'Мем-слайдер',
+        'proj7-text': '<span class="YBspan">YouTube</span> канал',
+
+        'about-text': 'Обо мне',
+        'preview-text': 'Abakkus — это коллекция проектов, в которых представлены работы, специализирующиеся на UI/UX/графическом дизайне, иллюстрации и анимационной графике. Я увлечен концепцией и созданием интерактивных приложений и убежден, что четкое удобство использования в сочетании со стимулирующей графикой может доставить убедительное сообщение. До сих пор мне повезло, что у меня была возможность работать с удивительно талантливыми людьми над действительно вдохновляющими проектами. Благодаря этому сотрудничеству я смог получить широкий спектр идей и знаний, которыми я хотел бы поделиться с вами здесь и в вашем следующем проекте.',
+        'contact-text': 'Контакты',
+
+        'designed-text': 'Разработано Павлом Терно'
+    }
+}
+
+export default i18Obj

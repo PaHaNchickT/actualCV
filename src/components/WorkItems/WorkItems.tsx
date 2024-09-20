fb8eb40f5053efc2c32fe3762b16b8732bb84dd5
Pavel <you@example.com>
2024-09-20 03:56:58 +0300

    feat: stylize items and items modal sections

--- src/components/WorkItems/WorkItems.tsx
+++ src/components/WorkItems/WorkItems.tsx
@@ -9,7 +9,6 @@ import {
   Modal,
   ModalBody,
   ModalContent,
-  ModalFooter,
   ModalHeader,
   useDisclosure,
 } from '@nextui-org/react';
@@ -30,7 +29,7 @@ const WorkItems = (props: { item: TWorkItem }): ReactElement => {
           shadow="sm"
           isPressable
           onPress={onOpen}
-          className="w-[400px] h-[400px] flex flex-col justify-center relative bg-black hover:bg-[#F6AD34]"
+          className="w-[400px] h-[400px] flex flex-col justify-center relative bg-black group/card"
         >
           <CardBody className="overflow-visible p-0 absolute z-[2] opacity-70">
             <Image
@@ -39,12 +38,11 @@ const WorkItems = (props: { item: TWorkItem }): ReactElement => {
               width="400px"
               height="400px"
               alt={`${t(`items.${props.item.id}.title`)}-cover`}
-              className="w-full object-cover blur-[1px] grayscale"
+              className="w-full object-cover blur-[1px] transition-all duration-300 grayscale group-hover/card:grayscale-0"
               src={`${ASSETS_PATH.proj}${props.item.id}.jpg`}
             />
           </CardBody>
-          <div className="bg-background px-6 py-3 text-3xl box-border relative z-[3]">
-            {/* {transition-all duration-300 group-hover/card:w-full group-hover/card:text-4xl} */}
+          <div className="bg-background pl-8 pr-2 py-1 text-xl box-border relative z-[3] transition-all min-w-[10px] group-hover/card:min-w-full group-hover/card:text-5xl group-hover/card:py-4 group-hover/card:px-0">
             <b>{t(`items.${props.item.id}.title`)}</b>
           </div>
         </Card>
@@ -58,7 +56,7 @@ const WorkItems = (props: { item: TWorkItem }): ReactElement => {
                 {t(`items.${props.item.id}.subtitle`)}
               </ModalHeader>
               <ModalBody>
-                <div className="flex flex-row justify-between gap-5">
+                <div className="flex flex-row justify-between gap-5 pb-14">
                   <section className="w-[60%] flex flex-col gap-3">
                     <p className="text-justify">{t(`items.${props.item.id}.descrMain`)}</p>
                     <p className="text-justify">{t(`items.${props.item.id}.descrSub`)}</p>
@@ -76,24 +74,28 @@ const WorkItems = (props: { item: TWorkItem }): ReactElement => {
                       </div>
                     </div>
                   </section>
-                  <Image
-                    shadow="sm"
-                    radius="lg"
-                    width="300px"
-                    height="300px"
-                    alt={`${t(`items.${props.item.id}.title`)}-cover`}
-                    className="self-center object-cover"
-                    src={`${ASSETS_PATH.proj}${props.item.id}.jpg`}
-                  />
+                  <div className="flex flex-col gap-5">
+                    <Image
+                      isBlurred
+                      shadow="sm"
+                      radius="lg"
+                      width="300px"
+                      height="300px"
+                      alt={`${t(`items.${props.item.id}.title`)}-cover`}
+                      className="object-cover"
+                      src={`${ASSETS_PATH.proj}${props.item.id}.jpg`}
+                    />
+                    <Button color="primary" onPress={onClose}>
+                      <Link href={props.item.link} className="text-black" target="_blank">
+                        {t('btnLink')}
+                      </Link>
+                    </Button>
+                  </div>
                 </div>
               </ModalBody>
-              <ModalFooter className="justify-center">
-                <Button color="primary" onPress={onClose}>
-                  <Link href={props.item.link} className="text-black" target="_blank">
-                    {t('btnLink')}
-                  </Link>
-                </Button>
-              </ModalFooter>
+              {/* <ModalFooter className="justify-center">
+                
+              </ModalFooter> */}
             </>
           )}
         </ModalContent>

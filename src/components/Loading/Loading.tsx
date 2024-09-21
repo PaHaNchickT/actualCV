6029fb65e9c2e0880e0c91f87c5ac44a5286a6f3
Pavel <you@example.com>
2024-09-21 17:41:27 +0300

    feat: implement loading comp

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { Spinner } from '@nextui-org/react';
+import { type ReactElement } from 'react';
+
+const Loading = (): ReactElement => {
+  return (
+    <div className="bg-[#111111] w-screen h-screen flex justify-center items-center">
+      <Spinner color="warning" size="lg" />
+    </div>
+  );
+};
+
+export default Loading;

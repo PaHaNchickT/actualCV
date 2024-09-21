7486c174a0a305970791d4105ef10314e9350061
Pavel <you@example.com>
2024-09-21 23:41:05 +0300

    feat: implement internationalization for about page

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Avatar, Button, Card, CardBody, CardHeader, Link } from '@nextui-org/react';
+import { useTranslations } from 'next-intl';
 import { useState, type ReactElement } from 'react';
 
 import { DIPLOMAS, EDUCATION, STACK } from '@/constants/about-constants';
@@ -8,6 +9,7 @@ import { ASSETS_PATH } from '@/constants/work-constants';
 
 const About = (): ReactElement => {
   const [counter, setCounter] = useState(1);
+  const t = useTranslations('About');
   console.log(counter);
 
   const linkEpam = (
@@ -23,7 +25,7 @@ const About = (): ReactElement => {
 
   return (
     <div className="w-full p-10 relative z-[2] flex flex-col items-center gap-10">
-      <h3 className="font-black text-5xl text-center">About me</h3>
+      <h3 className="font-black text-5xl text-center">{t('title')}</h3>
       <div className="flex flex-col gap-5 w-[800px]">
         <Card className="p-4 max-w-[800px]" shadow="lg">
           <CardBody className="overflow-visible py-2">
@@ -32,20 +34,15 @@ const About = (): ReactElement => {
                 <Avatar src={ASSETS_PATH.avatar} className="w-[200px] h-[200px] shadow-[0_0_20px_7px_background]" />
                 <div>
                   <p>
-                    <span className="text-primary mr-1">Возраст:</span> 25
+                    <span className="text-primary mr-1">{t('info.age')}</span> 25
                   </p>
                   <p>
-                    <span className="text-primary mr-1">Статус:</span> Ищу работу
+                    <span className="text-primary mr-1">{t('info.status')}</span>
+                    {t('info.current')}
                   </p>
                 </div>
               </div>
-              <p className="w-8/12 text-justify">
-                Фронтенд разработка для меня – это в первую очередь творчество, идеальный баланс между интеллектуальной
-                работой и выражением эмоций. Ввиду своей эмпатичности я успешно нахожу общий язык с людьми и легко
-                вливаюсь в коллектив. Стремлюсь решать задачи эффективно и с творческим подходом, поэтому планирую
-                достигнуть высоких показателей результативности работы. Обладаю следующими качествами:
-                целеустремленность, легко обучаемость, организаторские способности. Постоянно занимаюсь саморазвитием.
-              </p>
+              <p className="w-8/12 text-justify">{t('info.text')}</p>
             </div>
           </CardBody>
         </Card>
@@ -78,12 +75,7 @@ const About = (): ReactElement => {
                 </div>
               </div>
               <p className="w-8/12 text-justify">
-                Во время обучения в онлайн школах от {linkEpam} и {linkRSS} я приобрел навыки разработки приложений на
-                JavaScript, TypeScript и React. Среди них были как одностраничные приложения и игры, так и полноценные
-                многостраничные сайты, сверстанные по макетам Figma. При написании кода я чётко следую концепциям DRY,
-                KISS и YAGNI, а благодаря регулярным перекрестным проверкам работ, я имею уверенные навыки Code Review.
-                Во время финальных проектов я также изучил методологию Agile и приобрел опыт коллективной разработки по
-                Scrum, используя доски Jira и gh project.
+                {t('work.0')} {linkEpam} {t('work.1')} {linkRSS} {t('work.2')}
               </p>
             </div>
           </CardBody>
@@ -91,13 +83,13 @@ const About = (): ReactElement => {
         <div className="flex gap-5">
           <div className="flex flex-col gap-5">
             <Card className="p-4 max-w-[800px]" shadow="lg">
-              <CardHeader className="text-primary">Образование</CardHeader>
+              <CardHeader className="text-primary">{t('education.header')}</CardHeader>
               <CardBody className="overflow-visible py-2 pl-7">
                 <ul className="list-disc marker:text-primary">
                   {EDUCATION.map((item, index) => (
                     <li key={index}>
-                      <p>{item.name}</p>
-                      <p className="font-black">{item.degree}</p>
+                      <p>{t(item.name)}</p>
+                      <p className="font-black">{t(item.degree)}</p>
                       <p className="text-primary">{item.duration}</p>
                     </li>
                   ))}
@@ -105,29 +97,27 @@ const About = (): ReactElement => {
               </CardBody>
             </Card>
             <Card className="p-4 max-w-[800px]" shadow="lg">
-              <CardHeader className="text-primary">Языки</CardHeader>
+              <CardHeader className="text-primary">{t('languages.header')}</CardHeader>
               <CardBody className="overflow-visible py-2">
                 <p className="text-justify">
-                  Английский (<span className="text-primary">B2</span> - Upper-Intermediate)
+                  {t('languages.name')} (<span className="text-primary">B2</span> - Upper-Intermediate)
                 </p>
               </CardBody>
             </Card>
             <Card className="p-4 max-w-[800px]" shadow="lg">
-              <CardHeader className="text-primary">Хобби</CardHeader>
+              <CardHeader className="text-primary">{t('hobby.header')}</CardHeader>
               <CardBody className="overflow-visible py-2">
-                <p className="text-justify">
-                  В свободное время я играю на гитаре и ударных, а также веду свой YouTube канал.
-                </p>
+                <p className="text-justify">{t('hobby.text')}</p>
               </CardBody>
             </Card>
           </div>
           <div>
             <Card className="p-4 max-w-[800px] h-full" shadow="lg">
-              <CardHeader className="text-primary">Cтек</CardHeader>
+              <CardHeader className="text-primary">{t('stack.header')}</CardHeader>
               <CardBody className="overflow-visible py-2 pl-7 h-full">
                 <ul className="list-disc marker:text-primary h-full flex flex-col justify-between">
                   {STACK.map((item, index) => (
-                    <li key={index}>{item}</li>
+                    <li key={index}>{t(item)}</li>
                   ))}
                 </ul>
               </CardBody>
--- src/constants/about-constants.ts
+++ src/constants/about-constants.ts
@@ -1,15 +1,19 @@
 export const DIPLOMAS = ['', '', ''];
 
-export const EDUCATION = [
-  { name: 'Санкт-Петербургский государственный университет', degree: 'Бакалавриат', duration: '2017 - 2021' },
-  { name: 'Санкт-Петербургский государственный университет', degree: 'Магистратура', duration: '2021 - 2023' },
+export const EDUCATION: {
+  name: 'education.university';
+  degree: 'education.bachelor' | 'education.master';
+  duration: string;
+}[] = [
+  { name: 'education.university', degree: 'education.bachelor', duration: '2017 - 2021' },
+  { name: 'education.university', degree: 'education.master', duration: '2021 - 2023' },
 ];
 
-export const STACK = [
-  'Уверенное владение HTML, SASS, Tailwind, JavaScript, TypeScript, React, Redux + RTK Query, Webpack, Vite, NextUI (библиотека компонентов), NextIntl (интернационализация), React Hook Form, Zod и Yup (валидация форм)',
-  'Понимание концепции SSR, уверенное владение Next.js и опыт работы с Remix',
-  'Понимание концепции Rest API, опыт работы с Websocket и GraphQL запросами, а также с токенами аутентификации (FireBase)',
-  'Опыт написания юнит тестов на Jest',
-  'Базовые знания и опыт работы с Node.js',
+export const STACK: ['stack.0', 'stack.1', 'stack.2', 'stack.3', 'stack.4'] = [
+  'stack.0',
+  'stack.1',
+  'stack.2',
+  'stack.3',
+  'stack.4',
 ];
 

c1ac916d9381c00b47e518cad51158a4893285dd
Pavel <you@example.com>
2024-09-21 19:35:49 +0300

    feat: implement prototype of about page

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -2,13 +2,14 @@
 
 import type { ReactElement } from 'react';
 
+import About from '@/components/About/About';
 import Video from '@/components/Video/Video';
 
 const Home = (): ReactElement => {
   return (
     <div className="flex flex-col justify-center w-full relative">
       <Video />
-      <p className="relative z-[2]">About</p>
+      <About />
     </div>
   );
 };
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,11 +1,17 @@
 'use client';
 
+import { Card, CardBody } from '@nextui-org/react';
 import { type ReactElement } from 'react';
 
 const About = (): ReactElement => {
   return (
-    <div className="w-full p-20 relative z-[2]">
-      <h3 className="font-black text-6xl text-center">About</h3>
+    <div className="w-full p-10 relative z-[2] flex flex-col items-center gap-10">
+      <h3 className="font-black text-5xl text-center">About me</h3>
+      <Card className="py-4 w-9/12">
+        <CardBody className="overflow-visible py-2">
+          <p>Info</p>
+        </CardBody>
+      </Card>
     </div>
   );
 };

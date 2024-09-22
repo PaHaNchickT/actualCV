5980c7fc695f8d1a6e347ca8afc3ec65164713a0
Pavel <you@example.com>
2024-09-22 19:53:04 +0300

    feat: implement notifications

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -2,43 +2,71 @@
 
 // import { zodResolver } from '@hookform/resolvers/zod';
 import emailjs from '@emailjs/browser';
-import { Button, Input, Textarea } from '@nextui-org/react';
+import { Button, Input, Spinner, Textarea } from '@nextui-org/react';
 import type { useTranslations } from 'next-intl';
-import type { FormEvent, ReactNode } from 'react';
+import { useState, type FormEvent, type ReactNode } from 'react';
 import type { SubmitHandler } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
+import toast, { Toaster } from 'react-hot-toast';
 
 import type { TContactForm } from '@/types/types';
 
 function ContactForm(props: { t: ReturnType<typeof useTranslations<'Contact'>> }): ReactNode {
   const {
     register,
+    reset,
     handleSubmit,
     formState: { errors },
   } = useForm<TContactForm>({
     mode: 'onChange',
     // resolver: zodResolver(RestSchema(t)),
   });
+  const [sending, setSending] = useState(false);
+  console.log(props);
+
+  const toastStyler = (color: string): object => {
+    return {
+      style: {
+        border: `1px solid ${color}`,
+        padding: '16px',
+        color: 'white',
+        top: '100px',
+        backgroundColor: '#111111',
+        textAlign: 'center',
+        gap: '10px',
+      },
+      iconTheme: {
+        primary: color,
+        secondary: '#FFFAEE',
+      },
+    };
+  };
 
   const submit = async (data: TContactForm, event: FormEvent): Promise<void> => {
-    console.log(event, data, props.t('title'));
+    setSending(true);
 
     emailjs
       .sendForm('service_0hrkkvh', 'template_onpkbb5', event.target as HTMLFormElement, {
         publicKey: 'xVpyZi5OQr6MxzU7z',
       })
-      .then(
-        () => {
-          console.log('SUCCESS!');
-        },
-        (error) => {
-          console.log('FAILED...', error.text);
-        },
-      );
+      .then(() => {
+        reset();
+        toast.success('Letter sent successfully!\nI will contact you shortly', toastStyler('#59D22C'));
+      })
+      .catch((error) => {
+        console.log(error.text);
+        toast.error('Something went wrong!\nTry again later', toastStyler('#E8314E'));
+      })
+      .finally(() => {
+        setSending(false);
+      });
   };
 
   return (
     <div className="flex flex-col items-center pt-5 px-2 gap-2">
+      <div>
+        <Toaster position="bottom-right" />
+      </div>
       <form
         onSubmit={handleSubmit(submit as SubmitHandler<TContactForm>)}
         className="flex flex-col items-center gap-5 w-full min-w-[400px] sm:w-[70%]"
@@ -50,6 +78,7 @@ function ContactForm(props: { t: ReturnType<typeof useTranslations<'Contact'>> }
           className="w-full text-center"
           isInvalid={Boolean(errors.from_name)}
           errorMessage={errors.from_name?.message}
+          isDisabled={sending}
         />
         <div className="flex justify-between w-full gap-5">
           <div className="w-full">
@@ -60,6 +89,7 @@ function ContactForm(props: { t: ReturnType<typeof useTranslations<'Contact'>> }
               className="w-full text-center"
               isInvalid={Boolean(errors.from_email)}
               errorMessage={errors.from_email?.message}
+              isDisabled={sending}
             />
           </div>
           <div className="w-full">
@@ -70,6 +100,7 @@ function ContactForm(props: { t: ReturnType<typeof useTranslations<'Contact'>> }
               className="w-full text-center"
               isInvalid={Boolean(errors.from_company)}
               errorMessage={errors.from_company?.message}
+              isDisabled={sending}
             />
           </div>
         </div>
@@ -80,18 +111,23 @@ function ContactForm(props: { t: ReturnType<typeof useTranslations<'Contact'>> }
           className="w-full h-[100px]"
           isInvalid={Boolean(errors.message)}
           errorMessage={errors.message?.message}
+          isDisabled={sending}
         />
         <div className="p-5">
           <Input type="hidden" value="PaHaNchick" {...register('to__name')} />
-          <Button
-            size="lg"
-            type="submit"
-            color={(Object.keys(errors).length && 'danger') || 'primary'}
-            isDisabled={Boolean(Object.keys(errors).length)}
-            className="h-14"
-          >
-            Send
-          </Button>
+          {sending ? (
+            <Spinner color="warning" size="lg" className="h-[56px]" />
+          ) : (
+            <Button
+              size="lg"
+              type="submit"
+              color={(Object.keys(errors).length && 'danger') || 'primary'}
+              isDisabled={Boolean(Object.keys(errors).length)}
+              className="h-14 text-background"
+            >
+              Send
+            </Button>
+          )}
         </div>
       </form>
     </div>
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -26,7 +26,7 @@ export const Footer = (): ReactElement => {
   const t = useTranslations('Works');
 
   return (
-    <footer className="z-50 bg-transparent border-t-1 border-t-default-300 w-full flex justify-center backdrop-blur-lg backdrop-saturate-150">
+    <footer className="z-5 bg-transparent border-t-1 border-t-default-300 w-full flex justify-center backdrop-blur-lg backdrop-saturate-150">
       <div className="max-w-5xl w-full p-6 flex justify-between relative text-primary font-black">
         <p>{t('footerAuthor')}</p>
         <p className="w-[39px] absolute left-[calc((100%-39px)/2)] text-center">2024</p>

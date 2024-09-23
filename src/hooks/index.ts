6c4d017ee3f421bb28bbd92e5a295a2000479e2e
Pavel <you@example.com>
2024-09-23 05:19:31 +0300

    fix: delete old and unused files

--- src/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as useEncryption } from './useEncryption';
-
-export { default as useHistoryService } from './useHistoryService';
--- src/mock/setupTests.ts
+++
@@ -1,7 +0,0 @@
-import 'whatwg-fetch';
-import server from './server';
-import '@testing-library/jest-dom';
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());

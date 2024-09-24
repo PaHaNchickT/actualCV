21710cbe6026ba695813bfcbff81ba1c8a3586d7
Pavel <you@example.com>
2024-09-24 06:00:27 +0300

    refactor: delete temporary commented code lines from video comp

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,18 +1,11 @@
 'use client';
 
-// import { Image } from '@nextui-org/react';
 import { type ReactElement } from 'react';
 
 import { ASSETS_PATH } from '@/constants/work-constants';
 
 const Video = (): ReactElement => {
   return (
-    // <Image
-    //   src={ASSETS_PATH.video}
-    //   alt="background"
-    //   width="1440px"
-    //   className="fixed top-0 left-0 z-[0] blur-[2px] opacity-90 w-[1440px] max-w-[1440px]"
-    // />
     <video
       src={ASSETS_PATH.video}
       poster={ASSETS_PATH.videoPoster}

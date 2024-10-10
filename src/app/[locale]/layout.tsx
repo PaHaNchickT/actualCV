6ca8572fb0c1e259b5dc65127136751089d1f411
Pavel <you@example.com>
2024-10-10 21:47:16 +0300

    fix: return generateStaticParams func

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,14 +1,20 @@
 import '@/styles/globals.css';
 import type { Metadata } from 'next';
 import { Comfortaa } from 'next/font/google';
-import { getMessages } from 'next-intl/server';
+import { getMessages, unstable_setRequestLocale } from 'next-intl/server';
 import { type ReactElement } from 'react';
 
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
-import { type Locale } from '@/i18n';
+import { locales, type Locale } from '@/i18n';
 
 import Providers from '../providers';
+
+export function generateStaticParams(): {
+  locale: Locale;
+}[] {
+  return locales.map((locale) => ({ locale }));
+}
 
 export const metadata: Metadata = {
   title: "Pavel's Terno Portfolio",
@@ -24,6 +30,7 @@ const RootLayout = async ({
   children: React.ReactNode;
   params: { locale: Locale };
 }>): Promise<ReactElement> => {
+  unstable_setRequestLocale(locale);
   const messages = await getMessages({ locale });
 
   return (

f806c6d7f2011d3c8553cf19891221764a33a0f8
Pavel <you@example.com>
2024-10-11 00:34:31 +0300

    feat: implement styles for error page

--- src/app/[locale]/error.tsx
+++ src/app/[locale]/error.tsx
@@ -4,6 +4,7 @@ import { Button } from '@nextui-org/react';
 import { useTranslations } from 'next-intl';
 import type { ReactElement } from 'react';
 
+import Video from '@/components/Video/Video';
 import { useRouterIntl } from '@/navigation';
 
 const Error = (): ReactElement => {
@@ -11,8 +12,9 @@ const Error = (): ReactElement => {
   const t = useTranslations('Error');
 
   return (
-    <div className="flex justify-center">
-      <div className="flex flex-col gap-4 mt-4">
+    <div className="flex justify-center items-center h-screen text-center">
+      <Video />
+      <div className="relative z-[1] flex flex-col gap-4 mt-4">
         <p className="text-xl">{t('message')}</p>
         <Button onClick={() => router.push('/')}>{t('button')}</Button>
       </div>
